2474a6495a2b8871e976f3de6740d15eb36eebb0
zhakina90 <zhakina90@gmail.com>
2018-11-26 15:34:27 -0600

    Added username, time

--- assets/javascript/forum.js
+++ assets/javascript/forum.js
@@ -13,24 +13,40 @@ firebase.initializeApp(config);
 var database = firebase.database();
 $("#chatBtn").on("click", function (e) {
     e.preventDefault();
+    var time = Date.now();
     var userChat = $("#user-input")
         .val()
         .trim();
+    var userNameInput = $("#user-name").val().trim();
     var newChat = {
-        input: userChat
+        name: userNameInput,
+        input: userChat,
+        date: time
     };
     console.log(newChat);
     database.ref().push(newChat);
-    $("#user-input").val("")
+    $("#user-name").val("");
+    $("#user-input").val("");
+    $("#time-show").val("");
+
 
 });
+
+
 database.ref().on("child_added", function (childSnapshot) {
+    var userNameDisp = childSnapshot.val().name;
     var newInput = childSnapshot.val().input;
+    var currentTime = childSnapshot.val().time;
 
-    console.log(newInput);
+    currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 
-    $("#chat-display").append("<div class ='display-text'><p>" + newInput + "</p></div>");
+    console.log(currentTime);
+
+    $("#chat-display").append("<div class ='display-text'><p>" + userNameDisp + ":  " + newInput + "<br>" + currentTime + "</p></div>");
+
 })
+
+
 //need to create firebase
 // push the user input to firebases
 // get the information from firebase and display it on dom //
