586b011fae2f9c29f6ccbb13eca1fbc9fdfa1b1c
Shawn Ard <shawnduncanard@gmail.com>
2018-11-26 15:41:45 -0600

    contact

---
+++ assets/javascript/contact.js
@@ -0,0 +1,51 @@
+var config = {
+    apiKey: "AIzaSyATTX_RG7krqTtekWvE80CZt1_We4vzLnI",
+    authDomain: "brewtiful-7d460.firebaseapp.com",
+    databaseURL: "https://brewtiful-7d460.firebaseio.com",
+    projectId: "brewtiful-7d460",
+    storageBucket: "brewtiful-7d460.appspot.com",
+    messagingSenderId: "244126403566"
+  };
+  firebase.initializeApp(config);
+  var dataRef = firebase.database();
+
+
+
+$('#email-us').on("click", function () {
+    event.preventDefault();
+
+    var  first = $("#firstname-input").val().trim();
+    var last = $("#lastname-input").val().trim();
+    var phone = $("#phone-input").val().trim();
+    var email = $("#email-input").val().trim();
+    var comment = $("#comment-input").val().trim();
+
+    // Code for the push
+    dataRef.ref().push({
+
+     first: first,
+      last: last,
+      phone: phone,
+      email: email,
+      comment: comment,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+
+    //clear values after pushing to database
+    $("#firstname-input").val("");
+    $("#lastname-input").val("");
+    $("#phone-input").val("");
+    $("#email-input").val("");
+    $("#comment-input").val("");
+  
+
+    console.log(first)
+
+    var final = comment +" " +
+    first + " " + last +" "+
+    phone +" "+
+    email;
+
+    console.log("here")
+    window.location.href= `mailto:brewtifulApp@gmail.com?subject=App%20Comments&body=${final}`;
+})
