7e3044100814da25cd35c384ea5c9e67e053d2e7
Shawn Ard <shawnduncanard@gmail.com>
2018-11-27 14:49:49 -0600

    fixed drop down menu and attactch search menu to google api

--- assets/javascript/gmaps-function.js
+++ assets/javascript/gmaps-function.js
@@ -5,14 +5,11 @@ var userLatitude = "41.881832";
 var userLongitude = "-87.623177";
 
 var geocoordinates = "&latitude=" + userLatitude + "&longitude=" + userLongitude;
-
-$("#user-submit").on("click", function()){
-    $("#brewery-div").html("<iframe
-    width="300"
-    height="225"
-    frameborder="0" style="border:0"
-    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD2my8N7YcBl2BIR3v-IYBb3dqqHazJdBQ&q=Space+Needle,Seattle+WA" allowfullscreen>
-  </iframe>"
+console.log("maybe?")
+$("#user-submit").on("click", function(){
+  console.log("here") 
+  $("#brewery-div").html("<iframe src='https://www.google.com/maps/embed/v1/place?key=AIzaSyD2my8N7YcBl2BIR3v-IYBb3dqqHazJdBQ&q=Space+Needle,Seattle+WA' width='300' height='225' frameborder='0px' margin='20px 0px' allowfullscreen></iframe>")
+  console.log("here too")
 
 })
 
